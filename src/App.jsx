af77fa1bde81d65ec44fb283cc1a9a7ca8afbc40
sPridmore96 <93345198+sPridmore96@users.noreply.github.com>
2022-08-14 09:13:09 +0100

    Trying HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.scss';
 import Routing from './containers/Routing/Routing';
+import {HashRouter} from "react-router-dom"
+
 
 
 
 function App() {
   return (
+    <HashRouter basename='/'>
     <div className="App">
       <Routing/>
     </div>
+    </HashRouter>
   );
 }
 
